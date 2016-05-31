eb4af3f2bb6e24688c2983d2c9202e78597e5da3
Rohitt Vashishtha <aero31aero@gmail.com>
2016-05-31 16:52:07 +0530

    Made Command Listener Skeleton

--- bot.js
+++ bot.js
@@ -1,28 +1,21 @@
 var conf = require("./conf");
+var listen = require("./lib/listen");
 conf.updates = {
     enabled: true,
-	get_interval: 1000
+    get_interval: 1000
 };
 var Telegram = require('telegram-bot-api');
 var bot = new Telegram(conf);
 console.log("Bot started.");
+
 bot.on('message', function (message) {
-	var chat_id = message.chat.id;
+    var chat_id = message.chat.id;
+    //console.log(message);
+    // It'd be good to check received message type here
+    // And react accordingly
+    // We consider that only text messages can be received here
 
-	// It'd be good to check received message type here
-	// And react accordingly
-	// We consider that only text messages can be received here
-
-	bot.sendMessage({
-		chat_id: message.chat.id,
-		text: message.text || 'This message doesn\'t contain text :('
-	})
-	.then(function(message)
-	{
-		console.log(message);
-	})
-	.catch(function(err)
-	{
-		console.log(err);
-	});
+    bot.sendMessage(listen.process(message)).catch(function (err) {
+        console.log(err);
+    });
 });
