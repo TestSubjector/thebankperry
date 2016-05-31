0e72cb33bd8be1bfbec418d0a4803f4fd5a44445
Rohitt Vashishtha <aero31aero@gmail.com>
2016-05-31 22:53:04 +0530

    Turned botlogic into a module. Applied JS Patterns Refactoring.

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
+'use strict'
 var conf = require("./conf");
-var listen = require("./lib/listen");
+var botlogic = require("./lib/listen");
 conf.updates = {
     enabled: true,
     get_interval: 1000
@@ -9,13 +10,9 @@ var bot = new Telegram(conf);
 console.log("Bot started.");
 
 bot.on('message', function (message) {
-    var chat_id = message.chat.id;
-    //console.log(message);
-    // It'd be good to check received message type here
-    // And react accordingly
-    // We consider that only text messages can be received here
-
-    bot.sendMessage(listen.process(message)).catch(function (err) {
-        console.log(err);
-    });
+    botlogic(message, function sendMessage(msg) {
+        bot.sendMessage(msg).catch(function (err) {
+            console.log(err);
+        })
+    })
 });
