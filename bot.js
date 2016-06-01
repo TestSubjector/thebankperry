c0959650fbd01437fb3f357bc6c647b40b3de747
Nischay Pro <NischayPro@yahoo.co.in>
2016-06-01 16:32:51 +0530

    NewTes = Mike

--- bot.js
+++ bot.js
@@ -1,29 +1,21 @@
 'use strict'
 var conf = require("./conf");
 var botlogic = require("./lib/listen");
-
 conf.updates = {
     enabled: true,
     get_interval: 1000
 };
-
-var mike = require("./lib/mike");
-mike.listen(3000, function () {
-    console.log('Express: Mike listening on port 3000.');
-});
-
 var Telegram = require('telegram-bot-api');
 var bot = new Telegram(conf);
 console.log("Telegram: Bot started.");
-
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/thebankperry', function (err) {
-    if (err) {
-        console.log('MongoDB: Connection Error.', err);
-    } else {
-        console.log('MongoDB: Connection Successful.');
-    }
-});
+    mongoose.connect('mongodb://localhost/thebankperry', function (err) {
+        if (err) {
+            console.log('MongoDB: connection error', err);
+        } else {
+            console.log('MongoDB: connection successful');
+        }
+    });
 bot.on('message', function (message) {
     botlogic(message, function sendMessage(msg) {
         console.log("REPLY: ", msg.text);
