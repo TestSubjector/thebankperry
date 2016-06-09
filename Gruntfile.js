fd45876648dba108618cb8a05d060724827949e2
Rohitt Vashishtha <aero31aero@gmail.com>
2016-06-09 22:13:16 +0530

    117 Grunt Errors. Starting Refactoring.

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+var grunt = require('grunt');
+
+grunt.loadNpmTasks('grunt-contrib-jshint');
+
+grunt.initConfig({
+  jshint: {
+    all: ['Gruntfile.js', 'lib/**/*.js']
+  }
+});
+
+grunt.registerTask('world', 'world task description', function(){
+  console.log('hello world');
+});
+
+grunt.registerTask('hello', 'say hello', function(name){
+  if(!name || !name.length)
+    grunt.warn('you need to provide a name.');
+
+  console.log('hello ' + name);
+});
+
+grunt.registerTask('default', ['jshint']);
