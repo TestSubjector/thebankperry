df69011832d68c5aaba656a6bd9e54c3095a18dd
Rohitt Vashishtha <aero31aero@gmail.com>
2016-06-20 09:45:00 +0530

    Added js-beautify. This repo is now a testing ground for new tools.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,45 @@
 var grunt = require('grunt');
 
 grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-js-beautify');
 
 grunt.initConfig({
+  js_beautify: {
+    options: {
+      "end_with_newline": true,
+      "indent_size": 2,
+      "indent_char": " ",
+      "eol": "\n",
+      "indent_with_tabs": false,
+      "preserve_newlines": true,
+      "max_preserve_newlines": 10,
+      "jslint_happy": false,
+    },
+    files: ['lib/**/*.js', 'bot.js', 'Gruntfile.js']
+  },
+
   jshint: {
     all: ['Gruntfile.js', 'lib/**/*.js', 'bot.js']
   }
 });
 
-grunt.registerTask('world', 'world task description', function(){
+grunt.registerTask('world', 'world task description', function () {
   console.log('hello world');
 });
 
-grunt.registerTask('doc', 'generates static markdown documentation', function(){
+grunt.registerTask('doc', 'generates static markdown documentation', function () {
   require('mdoc').run({
     // configuration options (specified below)
     inputDir: 'doc',
     outputDir: 'dist'
-});
+  });
 });
 
-grunt.registerTask('hello', 'say hello', function(name){
-  if(!name || !name.length)
+grunt.registerTask('hello', 'say hello', function (name) {
+  if (!name || !name.length)
     grunt.warn('you need to provide a name.');
 
   console.log('hello ' + name);
 });
 
-grunt.registerTask('default', ['jshint']);
+grunt.registerTask('default', ['jshint', 'js_beautify']);
--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 var conf = require("./conf");
 var botconf = {
-    'token': conf.telegramToken,
-    'updates': {
-        enabled: true,
-        get_interval: 1000
-    }
+  'token': conf.telegramToken,
+  'updates': {
+    enabled: true,
+    get_interval: 1000
+  }
 }
 var botMessageProcessor = require("./lib/listen");
 
@@ -15,22 +15,22 @@ console.log("Telegram: Bot started.");
 var mike = require("./lib/mike");
 mike.setBroadcastBot(bot);
 mike.webServer.listen(3000, function () {
-    console.log('Express: Mike listening on port 3000.');
+  console.log('Express: Mike listening on port 3000.');
 });
 
 var mongoose = require('mongoose');
 mongoose.connect(conf.dbpath + conf.dbname, function (err) {
-    if (err) {
-        console.log('MongoDB: connection error', err);
-    } else {
-        console.log('MongoDB: connection successful');
-    }
+  if (err) {
+    console.log('MongoDB: connection error', err);
+  } else {
+    console.log('MongoDB: connection successful');
+  }
 });
 bot.on('message', function (message) {
-    botMessageProcessor(message, function(msg) {
-        console.log("REPLY: ", msg.text);
-        bot.sendMessage(msg).catch(function (err) {
-            console.log(err);
-        });
+  botMessageProcessor(message, function (msg) {
+    console.log("REPLY: ", msg.text);
+    bot.sendMessage(msg).catch(function (err) {
+      console.log(err);
     });
+  });
 });
